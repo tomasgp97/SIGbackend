497905d4c46df4a623412bf80a231354bcdd1514
tomasgp97 <tomas.gonzalezp97@gmail.com>
2022-08-12 14:12:15 -0300

    first commit

---
+++ src/main/java/com/austral/sigback/SiGbackApplication.java
@@ -0,0 +1,13 @@
+package com.austral.sigback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiGbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiGbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/austral/sigback/SiGbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.austral.sigback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiGbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
